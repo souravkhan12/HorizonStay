88e5ceb4cc728781e0d73a69e82b9a1577455bcb
Sourav khan <62439589+souravkhan12@users.noreply.github.com>
2025-02-01 19:56:40 +0530

    Added Compound Pattern & Render Pattern

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
         <Heading type="h1">All cabins</Heading>
-        <p>TEST</p>
+        <p>Filter / Sort</p>
       </Row>
 
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm(!showForm)}>Add New Cabin</Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
